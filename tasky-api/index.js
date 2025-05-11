21dba6f1dac178a81e2222379d837f0e44302f3b
Daniel Canning <canning932@gmail.com>
2025-05-11 21:05:28 +0100

    Mongo lab Integrated Tasky React App

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,13 +2,16 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+// other imports
+import cors from 'cors';
+
 
 dotenv.config();
 
 const errHandler = (err, req, res, next) => {
   /* if the error in development then send stack trace to display whole error,
   if it's in production then just send error message  */
-  if(process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     return res.status(500).send(`Something went wrong!`);
   }
   res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
@@ -18,6 +21,9 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 
 const port = process.env.PORT;
+
+// Enable CORS for all requests
+app.use(cors());
 
 app.use(express.json());
 
