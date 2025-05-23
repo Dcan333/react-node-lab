3973abd0f27b9118655ba0b9482d8afab6095010
Daniel Canning <canning932@gmail.com>
2025-05-23 20:53:19 +0100

    Added Auth Middleware

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -5,6 +5,7 @@ import './db';
 // other imports
 import cors from 'cors';
 import usersRouter from './api/users';
+import authenticate from './authenticate';
 
 
 dotenv.config();
@@ -28,7 +29,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', authenticate, tasksRouter);
 
 app.use('/api/users', usersRouter);
 
