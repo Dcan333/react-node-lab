c96c8ea8d2f72f877ae4428fcf354c67018844da
Daniel Canning <canning932@gmail.com>
2025-05-23 21:01:29 +0100

    Authentication lab - exercise completed

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -4,9 +4,21 @@ import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
+const passwordValidator = function(password) {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    return passwordRegex.test(password);
+};
+
 const UserSchema = new Schema({
     username: { type: String, unique: true, required: true },
-    password: { type: String, required: true }
+    password: { 
+        type: String, 
+        required: true,
+        validate: {
+            validator: passwordValidator,
+            message: 'Password must be at least 8 characters long and contain at least one letter, one digit, and one special character (@$!%*#?&)'
+        }
+    }
 });
 
 UserSchema.methods.comparePassword = async function (passw) {
